0f6a39f66559546c5aa3a27dec6fc169da7a9cd1
Peter Richardson <github@zoomy.net>
2016-01-08 15:49:10 -0500

    handle differing display densities

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ map = (function () {
 
     var layer = Tangram.leafletLayer({
         scene: 'scene.yaml',
-        postUpdate: postUpdate
+        postUpdate: postUpdate,
+        // highDensityDisplay: false
     });
 
     window.layer = layer;
@@ -66,16 +67,16 @@ map = (function () {
 }());
 
 // var img1Ctx, img1;
-var buf;
+var buf, mapImageURL, newimg;
+
 // Take a screenshot and save file
 function screenshot() {
     // Adapted from: https://gist.github.com/unconed/4370822
-    var image = scene.canvas.toDataURL('image/png').slice(22); // slice strips host/mimetype/etc.
+    mapImageURL = scene.canvas.toDataURL('image/png');
+    newimg = document.createElement('img');
+    newimg.src = mapImageURL;
 
-    // img1Ctx = scene.canvas.getContext("2d");
-    // console.log('img1Ctx:', img1Ctx);
-    // img1 = img1Ctx.getImageData(0, 0, 250, 250);
-
+    var image = mapImageURL.slice(22); // slice strips host/mimetype/etc.
 
     var data = atob(image); // convert base64 to binary without UTF-8 mangling
     buf = new Uint8Array(data.length);
@@ -89,27 +90,40 @@ function screenshot() {
 setTimeout(function() {
     queue_screenshot = true;
     scene.requestRedraw();
-}, 1000);
+}, 1500);
 
 setTimeout(function() {
 
+    // var oldimg = document.getElementById('myimg');
+    var oldimg = document.createElement('img');
+    oldimg.src = 'tangram-1452283152715.png';
+    var oldcanvas = document.createElement('canvas');
+    oldcanvas.height = 500;
+    oldcanvas.width = 500;
+    var oldCtx = oldcanvas.getContext('2d');
+    oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas.width, oldcanvas.height);
+    // oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas);
+    // oldCtx.drawImage(oldimg, 0, 0);
+    var oldData = oldCtx.getImageData(0, 0, 500, 500);
+
     var newcanvas = document.createElement('canvas');
-    var img2Ctx = newcanvas.getContext('2d');
-    var img = document.getElementById('myimg');
-    img2Ctx.drawImage(img, 0, 0 );
-    var img2 = img2Ctx.getImageData(0, 0, img.width, img.height);
+    newcanvas.height = 500;
+    newcanvas.width = 500;
+    var newCtx = newcanvas.getContext('2d');
+    newCtx.drawImage(newimg, 0, 0, newimg.width, newimg.height, 0, 0, newcanvas.width, newcanvas.height);
+    // newCtx.drawImage(newimg, 0, 0);
+    var newData = newCtx.getImageData(0, 0, 500, 500);
 
     var diffcanvas = document.createElement('canvas');
+    diffcanvas.height = 500;
+    diffcanvas.width = 500;
     var diffCtx = diffcanvas.getContext('2d');
+    var diff = diffCtx.createImageData(500, 500);
 
-
-    // var img1 = img1Ctx.getImageData(0, 0, 250, 250),
-    //     img2 = img2Ctx.getImageData(0, 0, 250, 250),
-    var diff = diffCtx.createImageData(250, 250);
-
-    pixelmatch(buf, img2.data, diff.data, 250, 250, {threshold: 0.1});
+    pixelmatch(newData.data, oldData.data, diff.data, 500, 500, {threshold: 0.1});
 
     diffCtx.putImageData(diff, 0, 0);
     document.getElementById("diff").appendChild(diffcanvas);
+    
 }, 2000);
 
