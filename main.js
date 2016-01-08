b430b7cb6c7a2a1f24292600474ba00308e5cc31
Peter Richardson <github@zoomy.net>
2016-01-08 16:43:25 -0500

    load and size images dynamically

--- main.js
+++ main.js
@@ -66,20 +66,26 @@ map = (function () {
 
 }());
 
-// var img1Ctx, img1;
-var buf, mapImageURL, newimg;
+// initialize variables
+var oldimg, newimg;
+
+// draw old image
+oldimg = document.createElement('img');
+oldimg.src = 'tangram-1452283152715.png';
+document.getElementById("old").appendChild(oldimg);
+
 
 // Take a screenshot and save file
 function screenshot() {
     // Adapted from: https://gist.github.com/unconed/4370822
-    mapImageURL = scene.canvas.toDataURL('image/png');
+    var mapImageURL = scene.canvas.toDataURL('image/png');
     newimg = document.createElement('img');
     newimg.src = mapImageURL;
 
     var image = mapImageURL.slice(22); // slice strips host/mimetype/etc.
 
     var data = atob(image); // convert base64 to binary without UTF-8 mangling
-    buf = new Uint8Array(data.length);
+    var buf = new Uint8Array(data.length);
     for (var i = 0; i < data.length; ++i) {
         buf[i] = data.charCodeAt(i);
     }
@@ -87,43 +93,49 @@ function screenshot() {
     saveAs(blob, 'tangram-' + (+new Date()) + '.png'); // uses FileSaver.js: https://github.com/eligrey/FileSaver.js/
 }
 
+// give the scene time to draw, then queue a screenshot
 setTimeout(function() {
     queue_screenshot = true;
     scene.requestRedraw();
+
 }, 1500);
 
+// and perform the image comparison
 setTimeout(function() {
 
-    // var oldimg = document.getElementById('myimg');
-    var oldimg = document.createElement('img');
-    oldimg.src = 'tangram-1452283152715.png';
+    // make a canvas for the old image
     var oldcanvas = document.createElement('canvas');
     oldcanvas.height = 500;
     oldcanvas.width = 500;
+    // add the old image to it
     var oldCtx = oldcanvas.getContext('2d');
     oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas.width, oldcanvas.height);
-    // oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas);
-    // oldCtx.drawImage(oldimg, 0, 0);
+    // make the data available
     var oldData = oldCtx.getImageData(0, 0, 500, 500);
 
+    // make a canvas for the newly-drawn map image
     var newcanvas = document.createElement('canvas');
     newcanvas.height = 500;
     newcanvas.width = 500;
     var newCtx = newcanvas.getContext('2d');
+    // draw map image so it fits the new canvas size (in case it's retina)
     newCtx.drawImage(newimg, 0, 0, newimg.width, newimg.height, 0, 0, newcanvas.width, newcanvas.height);
-    // newCtx.drawImage(newimg, 0, 0);
+    // make the data available
     var newData = newCtx.getImageData(0, 0, 500, 500);
 
+    // make a canvas for the diff
     var diffcanvas = document.createElement('canvas');
     diffcanvas.height = 500;
     diffcanvas.width = 500;
     var diffCtx = diffcanvas.getContext('2d');
     var diff = diffCtx.createImageData(500, 500);
 
+    // run the diff
     pixelmatch(newData.data, oldData.data, diff.data, 500, 500, {threshold: 0.1});
 
+    // put the diff in its canvas and attach it to the document for viewing
     diffCtx.putImageData(diff, 0, 0);
     document.getElementById("diff").appendChild(diffcanvas);
     
-}, 2000);
+}, 3000);
 
