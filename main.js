c6d3cf56e8f4a11d118448463ac51518f6b36ed4
Peter Richardson <github@zoomy.net>
2016-01-11 17:38:57 -0500

    use view_complete hook

--- main.js
+++ main.js
@@ -35,11 +35,11 @@ map = (function () {
 
     /*** Map ***/
 
-    var map = L.map('map',
-        {keyboardZoomOffset : .05,
-         zoomControl: false,
-         attributionControl : false}
-    );
+    var map = L.map('map', {
+        keyboardZoomOffset : .05,
+        zoomControl: false,
+        attributionControl : false
+    });
 
     var layer = Tangram.leafletLayer({
         scene: 'scene.yaml',
@@ -69,18 +69,19 @@ map = (function () {
 
 }());
 
-// draw old image
-var oldimg = new Image();
-// make a canvas for the old image once the image loads
+
+// set up canvases
+
+// make a canvas for the old image
 var oldcanvas = document.createElement('canvas');
 oldcanvas.height = size;
 oldcanvas.width = size;
 var oldCtx = oldcanvas.getContext('2d');
 
 // set the old image to be drawn to the canvas once the image loads
+var oldimg = new Image();
 oldimg.addEventListener('load', function () {
     oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas.width, oldcanvas.height);
-    // document.getElementById("old").appendChild(oldcanvas);
     // make the data available to pixelmatch
     oldData = oldCtx.getImageData(0, 0, size, size);
 });
@@ -102,26 +103,28 @@ var diff = diffCtx.createImageData(size, size);
 
 
 
-// Take a screenshot and save file
+// take a screenshot
 function screenshot() {
+    console.log('screenshot');
     return scene.screenshot().then(function(screenshot) {
-        saveAs(screenshot.blob, 'tangram-' + (+new Date()) + '.png');
+        // // save it to a file
+        // saveAs(screenshot.blob, 'tangram-' + (+new Date()) + '.png');
 
         var urlCreator = window.URL || window.webkitURL;
-        newimg = document.createElement('img');
+        newimg = new Image();
         newimg.src = urlCreator.createObjectURL( screenshot.blob );
     });
 }
 
 // give the scene time to draw, then queue a screenshot
-setTimeout(function() {
-    screenshot();
-}, 1500);
+// setTimeout(function() {
+//     screenshot();
+// }, 1500);
 
-// and perform the image comparison
-setTimeout(function() {
+// perform the image comparison
+function doDiff() {
 
-    // draw map image so it fits the new canvas size (in case it's retina)
+    // once the map is done drawing, save it to the new canvas, stretching it to fit (in case it's retina)
     newCtx.drawImage(newimg, 0, 0, newimg.width, newimg.height, 0, 0, newcanvas.width, newcanvas.height);
     // make the data available
     var newData = newCtx.getImageData(0, 0, size, size);
@@ -133,14 +136,62 @@ setTimeout(function() {
     diffCtx.putImageData(diff, 0, 0);
 
     // make imgs for new, old, and diff and attach them to the document
+    newimg.width = size;
+    newimg.height = size;
+    document.getElementById("new").insertBefore( newimg, document.getElementById("new").firstChild );
+
     oldimg.width = size;
     oldimg.height = size;
     document.getElementById("old").insertBefore( oldimg, document.getElementById("old").firstChild );
-
 
     diffimg = document.createElement('img');
     diffimg.src = diffcanvas.toDataURL("image/png");
     document.getElementById("diff").insertBefore( diffimg, document.getElementById("diff").firstChild );
     
-}, 2000);
+};
 
+
+var views = [
+    ['http://localhost:7000/eraser-map.yaml', 48.86110101269274, 2.361373901367188, 11],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 40.7139883550567, -74.00600910186769, 15],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 51.50286581276559,-0.12119293212890626,14],
+    ['http://localhost:7000/eraser-map.yaml', 35.68518697509636,139.75725173950198,15],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 47.604774168947614,-122.28607177734376,11],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 41.89075864654001,12.487063873882976,17]
+];
+var v;
+
+function nextView () {
+    v = v || 0;
+    if (v < views.length) {
+        var view = views[v];
+        if (scene.config_path !== view[0]) {
+            scene.load(view[0]).then(function() {
+                map.setView([view[1], view[2]], view[3]);
+                scene.requestRedraw();
+                v++;
+            });
+        }
+        else {
+            map.setView([view[1], view[2]], view[3]);
+            scene.requestRedraw();
+            v++;
+        }
+    }
+}
+
+scene.subscribe({
+    view_complete: function () {
+        console.log('complete');
+        if (!(v > views.length)) {
+            screenshot().then(function() {
+                doDiff();
+                console.log('done');
+                // nextView();
+            });
+        }
+    }
+});
+
+scene.resetViewComplete();
+// nextView(); // kick off the process
