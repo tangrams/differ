357ffa4293a7e239711e42a1ed37c8bcd9dbe3eb
Peter Richardson <github@zoomy.net>
2016-01-13 16:25:25 -0500

    return promise values properly

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+"use strict";
 /*jslint browser: true*/
 /*global Tangram, gui */
 
 // initialize variables
-var newimg, newData, oldData, size = 250;
+var newimg, oldData, size = 250;
 
 // set sizes
 document.getElementById("map").style.height = size+"px";
@@ -15,9 +16,7 @@ document.getElementsByClassName("container")[0].style.height = size+"px";
 document.getElementsByClassName("container")[0].style.width = size+"px";
 
 
-map = (function () {
-    'use strict';
-
+var map = (function () {
     var map_start_location = [40.70531887544228, -74.00976419448853, 15]; // NYC
 
     /*** URL parsing ***/
@@ -61,9 +60,9 @@ map = (function () {
 }());
 
 // load an image asynchronously with a Promise
-function loadImage (url) {
+function loadImage (url, target) {
     return new Promise(function(resolve, reject) {
-        var image = new Image();
+        var image = target || new Image();
         image.onload = function() {
             resolve({ url: url, image: image });
         };
@@ -85,19 +84,20 @@ var oldCtx = oldcanvas.getContext('2d');
 
 // set the old image to be drawn to the canvas once the image loads
 var oldimg = new Image();
-loadOld = new Promise(function(resolve, reject) {
-    console.log('loadOld');
+ //new Promise(function(resolve, reject) {
+    //console.log('loadOld');
 
-    loadImage('tangram-1452283152715.png').then(function(result){
+var loadOld = loadImage('tangram-1452283152715.png').then(function(result){
         oldimg = result.image;
         oldCtx.drawImage(oldimg, 0, 0, oldimg.width, oldimg.height, 0, 0, oldcanvas.width, oldcanvas.height);
         // make the data available to pixelmatch
         oldData = oldCtx.getImageData(0, 0, size, size);
         // console.log('load done');
 
-        resolve(); 
+        return result;
+        //resolve(); 
     });
-});
+//});
 
 // make a canvas for the newly-drawn map image
 var newcanvas = document.createElement('canvas');
@@ -112,36 +112,28 @@ diffcanvas.width = size;
 var diffCtx = diffcanvas.getContext('2d');
 var diff = diffCtx.createImageData(size, size);
 
-var newimg = new Image();
-
 // take a screenshot
-screenshot = new Promise (function(resolve, reject) {
-    console.log('screenshot');
-    return scene.screenshot().then(function(screenshot) {
+function screenshot () {
+    return scene.screenshot().then(function(data) {
         // save it to a file
-        // saveAs(screenshot.blob, 'tangram-' + (+new Date()) + '.png');
+        // saveAs(data.blob, 'tangram-' + (+new Date()) + '.png');
 
         var urlCreator = window.URL || window.webkitURL;
-        newimg.onload = function() {
-            // console.log('screenshot done');
-            // once the map is done drawing, save it to the new canvas, stretching it to fit (in case it's retina)
-            document.getElementById("new").insertBefore( newimg, document.getElementById("new").firstChild );
-            newCtx.drawImage(newimg, 0, 0, newimg.width, newimg.height, 0, 0, newcanvas.width, newcanvas.height);
-                // make the data available
-            newData = newCtx.getImageData(0, 0, size, size);
-                resolve();
-        };
-
-        newimg.src = urlCreator.createObjectURL( screenshot.blob );
-        console.log(newimg.src);
+        newimg = new Image();
+        return loadImage(urlCreator.createObjectURL( data.blob ), newimg);
     });
-});
+}
 
 // perform the image comparison
 function doDiff() {
-    console.log('doDiff');
+
+    // once the map is done drawing, save it to the new canvas, stretching it to fit (in case it's retina)
+    newCtx.drawImage(newimg, 0, 0, newimg.width, newimg.height, 0, 0, newcanvas.width, newcanvas.height);
+    // make the data available
+    var newData = newCtx.getImageData(0, 0, size, size);
+
     // run the diff
-    pixelmatch(newData.data, oldData.data, diff.data, size, size, {threshold: 0.3});
+    pixelmatch(newData.data, oldData.data, diff.data, size, size, {threshold: 0.125});
 
     // put the diff in its canvas
     diffCtx.putImageData(diff, 0, 0);
@@ -155,50 +147,49 @@ function doDiff() {
     oldimg.height = size;
     document.getElementById("old").insertBefore( oldimg, document.getElementById("old").firstChild );
 
-    diffimg = document.createElement('img');
+    var diffimg = document.createElement('img');
     diffimg.src = diffcanvas.toDataURL("image/png");
     document.getElementById("diff").insertBefore( diffimg, document.getElementById("diff").firstChild );
     
 };
 
 
-// var views = [
-//     ['http://localhost:7000/eraser-map.yaml', 48.86110101269274, 2.361373901367188, 11],
-//     ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 40.7139883550567, -74.00600910186769, 15],
-//     ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 51.50286581276559,-0.12119293212890626,14],
-//     ['http://localhost:7000/eraser-map.yaml', 35.68518697509636,139.75725173950198,15],
-//     ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 47.604774168947614,-122.28607177734376,11],
-//     ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 41.89075864654001,12.487063873882976,17]
-// ];
-// var v;
+var views = [
+    ['http://localhost:7000/eraser-map.yaml', 48.86110101269274, 2.361373901367188, 11],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 40.7139883550567, -74.00600910186769, 15],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 51.50286581276559,-0.12119293212890626,14],
+    ['http://localhost:7000/eraser-map.yaml', 35.68518697509636,139.75725173950198,15],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/tron.yaml', 47.604774168947614,-122.28607177734376,11],
+    ['https://raw.githubusercontent.com/tangrams/tangram-sandbox/gh-pages/styles/blueprint.yaml', 41.89075864654001,12.487063873882976,17]
+];
+var v;
 
-// function nextView () {
-//     v = v || 0;
-//     if (v < views.length) {
-//         var view = views[v];
-//         if (scene.config_path !== view[0]) {
-//             scene.load(view[0]).then(function() {
-//                 map.setView([view[1], view[2]], view[3]);
-//                 scene.requestRedraw();
-//                 v++;
-//             });
-//         }
-//         else {
-//             map.setView([view[1], view[2]], view[3]);
-//             scene.requestRedraw();
-//             v++;
-//         }
-//     }
-// }
+function nextView () {
+    v = v || 0;
+    if (v < views.length) {
+        var view = views[v];
+        if (scene.config_path !== view[0]) {
+            scene.load(view[0]).then(function() {
+                map.setView([view[1], view[2]], view[3]);
+                scene.requestRedraw();
+                v++;
+            });
+        }
+        else {
+            map.setView([view[1], view[2]], view[3]);
+            scene.requestRedraw();
+            v++;
+        }
+    }
+}
 
 scene.subscribe({
     view_complete: function () {
-        // console.log('view complete');
-        // if (!(v > views.length)) {
-            Promise.all([screenshot, loadOld]).then(function() {
-                doDiff();
-                // nextView();
-            });
-        // }
+        // console.log('complete');
+        Promise.all([screenshot(),loadOld]).then(function() {
+            console.log('all done!');
+            doDiff();
+            // nextView();
+        });
     }
 });
